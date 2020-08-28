07c6272dfe37fae3be6b853a985a0392413e4845
dmccoystephenson <dmccoystephenson@gmail.com>
2020-08-27 23:51:12 -0600

    created an instance of Command Subsystem in the main class

--- src/main/java/detectionsystem/AlternateAccountFinder.java
+++ src/main/java/detectionsystem/AlternateAccountFinder.java
@@ -1,17 +1,19 @@
 package detectionsystem;
 
+import detectionsystem.Subsystems.CommandSubsystem;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AlternateAccountFinder extends JavaPlugin {
 
+    CommandSubsystem commands = new CommandSubsystem(this);
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
--- src/main/java/detectionsystem/Subsystems/CommandSubsystem.java
+++ src/main/java/detectionsystem/Subsystems/CommandSubsystem.java
@@ -1,4 +1,13 @@
 package detectionsystem.Subsystems;
 
+import detectionsystem.AlternateAccountFinder;
+
 public class CommandSubsystem {
+
+    AlternateAccountFinder main = null;
+
+    public CommandSubsystem(AlternateAccountFinder plugin) {
+        main = plugin;
+    }
+
 }
