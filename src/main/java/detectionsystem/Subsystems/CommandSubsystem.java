faf9d1d1fa5003eff5c21a4bcb30856573d9411e
dmccoystephenson <dmccoystephenson@gmail.com>
2020-08-28 00:57:01 -0600

    final tweaks

--- src/main/java/detectionsystem/Subsystems/CommandSubsystem.java
+++ src/main/java/detectionsystem/Subsystems/CommandSubsystem.java
@@ -1,6 +1,8 @@
 package detectionsystem.Subsystems;
 
 import detectionsystem.AlternateAccountFinder;
+import detectionsystem.Commands.ListCommand;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class CommandSubsystem {
@@ -12,7 +14,14 @@ public class CommandSubsystem {
     }
 
     public boolean interpretCommand(CommandSender sender, String label, String[] args) {
-
+        switch(label) {
+            case "aaflist":
+                ListCommand command = new ListCommand(main);
+                command.showInfo(sender, args);
+                break;
+            default:
+                sender.sendMessage(ChatColor.RED + "AlternateAccountFinder doesn't know that command");
+        }
 
         return false;
     }
