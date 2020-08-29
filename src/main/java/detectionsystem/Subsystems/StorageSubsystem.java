9686068f3954a51782b581e6a7c4b1d46a231aeb
dmccoystephenson <dmccoystephenson@gmail.com>
2020-08-28 18:35:01 -0600

    created storage subsystem and passed an instance of main into it

---
+++ src/main/java/detectionsystem/Subsystems/StorageSubsystem.java
@@ -0,0 +1,13 @@
+package detectionsystem.Subsystems;
+
+import detectionsystem.AlternateAccountFinder;
+
+public class StorageSubsystem {
+
+    AlternateAccountFinder main = null;
+
+    public StorageSubsystem(AlternateAccountFinder plugin) {
+        main = plugin;
+    }
+
+}
